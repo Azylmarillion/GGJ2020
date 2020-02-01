e283fa48faf7d96945feeba03294d71a4d1f8100
Will <william.comminges@gmail.com>
2020-02-01 19:32:19 +0100

    Glitch upload

--- Assets/Will/Scripts/C#/EditorBase/EditorNeon.cs
+++ Assets/Will/Scripts/C#/EditorBase/EditorNeon.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class EditorNeon : MonoBehaviour
 {
     #region F/P
-    [SerializeField]
+    [SerializeField, Space]
     Image image = null;
 
     [SerializeField, Space]
