449c081c8b4884efa7480a2638e740740ddcbcac
Will <william.comminges@gmail.com>
2020-02-02 00:02:51 +0100

    debut cursor change

--- GGJ2020/Assets/Script/SpritesCollection/PointerSprites.cs
+++ GGJ2020/Assets/Script/SpritesCollection/PointerSprites.cs
@@ -1,7 +1,17 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
-[CreateAssetMenu(menuName = "sprite/pointerSprite")]
-public class PointerSprites : SpritesCollection
-{  
+[CreateAssetMenu(menuName = "Texture2D/pointerSprite")]
+public class PointerSprites : ScriptableObject
+{
+    [SerializeField] 
+        protected Texture2D _egypt;
+    [SerializeField] 
+        protected Texture2D _renaissance;
+    [SerializeField] 
+        protected Texture2D _samourai;
+    [SerializeField] 
+        protected Texture2D _hackerman;
+
+    public List<Texture2D> list => new List<Texture2D>() { _egypt, _renaissance, _samourai, _hackerman };
 }
-
---
+++ GGJ2020/Assets/Will/Scripts/C#/CursorManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorManager : ChangeOnTimeShift
+{
+    #region F/P
+    [SerializeField]
+    PointerSprites pointerSpritesCollection = null;
+
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    public override void OnTimeShift()
+    {
+
+    }
+}
